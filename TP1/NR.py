3955d4134a9b142c6c31d289d134a83b0e79eae8
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-06 02:05:27 -0300

    Formateo un poco el codigo y lo pongo un toque mas decente

--- TP1/NR.py
+++ TP1/NR.py
@@ -4,50 +4,52 @@ import sys
 ERROR_LIMIT = 0.00000000002
 MAX_ITERATIONS = 1000
 
+
 class Function(object):
-    
-    def eval(self,l):
+    def eval(self, l):
         pass
 
-class Original(Function):
 
-    def eval(self,l):
-        return (l**2)-(8*l)+16
+class Original(Function):
+    def eval(self, l):
+        return (l ** 2) - (8 * l) + 16
+
 
 class Derived(Function):
+    def eval(self, l):
+        return (2 * l) - 8
 
-    def eval(self,l):
-        return (2*l)-8
 
-def main(argv):
+def resolve_by_nr(f, ff,x0):
+    i = 0
+    error = 1
 
-    x0=argv[1]/2.0
-    i=0
-    error=1
+    while (error > ERROR_LIMIT) and (i < MAX_ITERATIONS):
+        f0 = f.eval(x0)
+        f1 = ff.eval(x0)
 
-    f = Original()
-    ff = Derived()
+        x1 = x0 - (f0 / f1)
 
-    while (error>ERROR_LIMIT) and (i<MAX_ITERATIONS):
-        f0=f.eval(x0)
-        f1=ff.eval(x0)
+        i += 1
 
-        x1=x0-(f0/f1)
+        error = abs((x1 - x0) / x1)
 
-        i+=1
-
-        error=abs((x1-x0)/x1);
-        
         x0 = x1
 
         if f.eval(x1) == 0:
-            error = 0;
-            break     
-            
-    print "Root: ", x0
-    print "Iterations: ", i
-    print "Error: ", error
-    print "Value: ", f.eval(x0)
+            error = 0
+            break
 
-#main(sys.argv)
-main(["",11])
+    return x0, i, error
+
+
+def main(args):
+    function = Original()
+    derived_function = Derived()
+
+    x0 = args[1] / 2.0
+
+    root, iterations, error = resolve_by_nr(function, derived_function,x0)
+
+# main(sys.argv)
+main(["", 11])
