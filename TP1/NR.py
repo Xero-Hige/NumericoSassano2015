f86e2694b05207b252d1a1b67b162ae5040d1e45
Tomas <tomasmussi@gmail.com>
2015-05-10 16:48:21 -0300

    Cambie las funciones y sus derivadas en base a lo que obtuve yo (Tomas) con Dani. Hice un par de correcciones para que si no se le pasa un argumento por consola no reviente y pueda ejecutarse (dado que Sassano seguramente lo ejecute sin parametros). Tambien algunos cambios de formato en la impresion de resultados y agregue una tablita para que vaya imprimiendo todas las iteraciones. Lo parametrice de una forma medio chota pero bueno... feel free to change it :P.

--- TP1/NR.py
+++ TP1/NR.py
@@ -1,83 +1,82 @@
 import math
 import sys
 
-ERROR_LIMIT = 0.00000000002
+#ERROR_LIMIT = 0.00000000002
+ERROR_LIMIT = 0.001
 MAX_ITERATIONS = 1000
-
+PI = 3.1416
 
 class Function(object):
-    def eval(self, l):
-        pass
+	def eval(self, l):
+		pass
 
-
-class Original(Function):
-    def eval(self, l):
-        return (l ** 3) - (8 * (l**2) ) + 16
-
+class Original(Function):	
+	def eval(self, l, L = 100):
+		return (1 / (64 * (PI**3))) * ((L - 4 * l) ** 4) + (l ** 4) / 12
 
 class Derived(Function):
-    def eval(self, l):
-        return (3 * (l**2) ) - (16*l)
+	def eval(self, l, L = 100):
+		return (-1 / (4 * (PI **3))) * ((L - 4 * l) ** 3) + (l ** 3) / 3
 
 class Second_Derived(Function):
-    def eval(self, l):
-        return (6*l) - 16
+	def eval(self, l, L = 100):
+		return (3 / (PI**3)) * ((L - 4 * l) ** 2) + (l ** 2)
 
 class Relation(Function):
-    def eval(self, l):
-        return 100 - l
+	def eval(self, l):
+		return 100 - l
 
-def resolve_by_nr(f, ff, x0):
-    i = 0
-    error = 1
+def resolve_by_nr(f, ff, x0,mostrar = False):
+	i = 0
+	error = 1
+	if(mostrar):
+		print "Xi+1		Xi		Error"
 
-    while (error > ERROR_LIMIT) and (i < MAX_ITERATIONS):
-        f0 = f.eval(x0)
-        f1 = ff.eval(x0)
+	while (error > ERROR_LIMIT) and (i < MAX_ITERATIONS):
+		f0 = f.eval(x0)
+		f1 = ff.eval(x0)
 
-        x1 = x0 - (f0 / f1)
+		x1 = x0 - (f0 / f1)
+		i += 1
+		error = abs((x1 - x0) / x1)
+		if (mostrar):
+			print "%.4f		%.4f		%.4f" % (x1, x0, error)			
 
-        i += 1
+		x0 = x1
 
-        error = abs((x1 - x0) / x1)
-
-        x0 = x1
-
-        if f.eval(x1) == 0:
-            error = 0
-            break
-
-    return x0, i, error
+		if f.eval(x1) == 0:
+			error = 0
+			break
+	return x0, i, error
 
 
 def main(args):
-    function = Original()
-    derived_function = Derived()
-    second_derived_function = Second_Derived()
-    relation = Relation()
+	function = Original()
+	derived_function = Derived()
+	second_derived_function = Second_Derived()
+	relation = Relation()
 
-    x0 = int(args[1]) / 2.0
+	if (len(sys.argv) > 1):
+		x0 = int(args[1]) / 2.0
+	else:
+		x0 = 50
+	print "Trabajando con x0 = %.4f y error: %.4f" % (x0, ERROR_LIMIT)
 
-    # Busco el 0 de la derivada, aka el punto critico
-    root, iterations, error = resolve_by_nr(derived_function, second_derived_function, x0)
+	# Busco el 0 de la derivada, aka el punto critico
+	root, iterations, error = resolve_by_nr(derived_function, second_derived_function, x0, True)
 
-    # TODO: Ver por si tiene varias raices (no raices multiples)
+	# Punto del minimo hallado
+	min_x = root
 
-    # Punto del minimo hallado
-    min_x = root
+	if (function.eval(min_x) > function.eval(0)):
+		min_x = 0  #Este extremo es seguro mejor que el punto encontrado
 
-    if (function.eval(min_x) > function.eval(0)):
-        min_x = 0  #Este extremo es seguro mejor que el punto encontrado
+	if (function.eval(min_x) > function.eval(x0 * 2)):
+		min_x = x0 * 2  #Este es el minimo
 
-    if (function.eval(min_x) > function.eval(x0 * 2)):
-        min_x = x0 * 2  #Este es el minimo
-
-    print 'Min at:', min_x
-    print 'Min value:', function.eval(min_x)
-
-    print 'L = ', min_x
-    print 'R = ', relation.eval(min_x)
-
-    #Results: https://www.wolframalpha.com/input/?i=%28l+**+3%29+-+%288+*+%28l**2%29+%29+%2B+16%3D-59.8518518519
+	print 'Minimo en x = %.4f' % (min_x)
+	print 'Valor del minimo: %.4f' % (function.eval(min_x))
+	print 'L = %.4f' % (min_x)
+	print 'R = %.4f' % (relation.eval(min_x))
 
 main(sys.argv)
